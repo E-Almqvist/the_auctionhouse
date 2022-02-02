191f7468fa0abc6c15737c435c8643c94d6b0c63
E. Almqvist <elalmqvist@gmail.com>
2022-02-02 15:56:04 +0100

    Minor refactor

--- src/app.rb
+++ src/app.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/ruby -w
 
 require "sinatra"
-if ARGV[0] == "debug" then
-	puts "Running app in debug mode..."
-	require "sinatra/reloader"
-end
+require "sinatra/reloader" if ARGV[0] == "debug"
 require "slim"
 require "sqlite3"
 
 get "/" do
-	"Test"
+	"Test tesasdfhsdhjkft"
 end
