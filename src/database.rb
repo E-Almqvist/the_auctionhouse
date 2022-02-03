c4a3be6f73a975b1b01b8d0696e1dd8781b5aef8
E. Almqvist <elalmqvist@gmail.com>
2022-02-03 13:08:07 +0100

    Stuff

--- src/database.rb
+++ src/database.rb
@@ -1,5 +1,7 @@
 DEFAULT_DB_PATH = "db/main.db"
 
+require_relative "db_models.rb"
+
 def db_handle 
 	db = SQLite3::Database.new DEFAULT_DB_PATH
 	db.results_as_hash = true
---
+++ src/db_models.rb
@@ -0,0 +1,6 @@
+class TableModel 
+	attr_reader :table_name
+	def initialize(table_name)
+		@table_name = table_name
+	end
+end
