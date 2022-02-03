c4c30f1f86246e5e2b39a005329e6d7adb889101
E. Almqvist <elalmqvist@gmail.com>
2022-02-03 13:27:29 +0100

    Table model etc

--- src/db_models.rb
+++ src/db_models.rb
@@ -1,6 +1,18 @@
-class TableModel 
+class TableModel  # Base model
 	attr_reader :table_name
 	def initialize(table_name)
 		@table_name = table_name
 	end
+
+	private def get(entity, filter="")
+		db = db_handle # get the db handle
+		query = "SELECT #{entity} FROM #{@table_name} " # create the query string
+		if filter != "" then query += "WHERE #{filter}" end
+
+		db.execute query
+	end
+end
+
+
+class User < TableModel
 end
