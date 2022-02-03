1e3eeafc01e529992c75eb3a8b7387881aa0b6c6
E. Almqvist <elalmqvist@gmail.com>
2022-02-03 12:54:51 +0100

    Removed unneeded db class

--- src/database.rb
+++ src/database.rb
@@ -1,15 +1,7 @@
-DEFAULT_DB_PATH = "db/database.db"
+DEFAULT_DB_PATH = "db/main.db"
 
-class Database
-	attr_reader :name, :db_path
-	def initialize(name, db_path=DEFAULT_DB_PATH)
-		@name = name
-		@db_path = db_path 
-	end
-
-	def get_handle 
-		db = SQLite3::Database.new @db_path
-		db.results_as_hash = true
-		return db
-	end
+def db_handle 
+	db = SQLite3::Database.new DEFAULT_DB_PATH
+	db.results_as_hash = true
+	return db
 end
