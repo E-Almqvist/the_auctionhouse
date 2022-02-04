3ac5eb941a477f975b3c81afcb41eab8ec2b90bb
E. Almqvist <elalmqvist@gmail.com>
2022-02-04 10:02:13 +0100

    Forms

--- src/app.rb
+++ src/app.rb
@@ -9,13 +9,20 @@ require "sassc"
 require_relative "database.rb"
 require_relative "func.rb"
 
-get "/" do
-	slim :index
-end
+enable :sessions
 
 get "/style.css" do
 	sass :"stylesheets/style", style: :compressed
 end
 
+get "/" do
+	slim :index
+end
 
+get "/login" do
+	slim :"user/login"
+end
 
+get "/register" do
+	slim :"user/register"
+end
